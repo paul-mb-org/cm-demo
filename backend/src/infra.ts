b1a869cf7f5c9624fcf5191551a298d7727f736c
Paweł Rugała <pawelrugala@Pawes-MacBook-Pro.local>
2026-02-12 08:57:04 +0100

    pr pipeline

--- backend/src/infra.ts
+++ backend/src/infra.ts
@@ -3,6 +3,8 @@ import { RestApiStack } from "./stacks/rest-api.stack";
 import { OrdersApiStack } from "./services/orders/orders.stack";
 
 const app = new App();
+const suffix = app.node.tryGetContext('envSuffix') ?? '';
+const stackSuffix = suffix ? `-${suffix}` : '';
 
-new RestApiStack(app, "ExternalApi");
-new OrdersApiStack(app, "OrdersApi");
+new RestApiStack(app, `ExternalApi${stackSuffix}`);
+new OrdersApiStack(app, `OrdersApi${stackSuffix}`);
